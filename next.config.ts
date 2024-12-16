174004c9ad1a913fc05fb23952f48f66433a464b
Hyuni Kim <spaceship00@naver.com>
2024-12-16 07:56:05 +0000

    Add github pages settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  basePath: '/match_table_validator',
+}
 
 export default nextConfig;
