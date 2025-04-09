511a5c8d8db01779cf150701fd645bec20e25d30
Hyuni Kim <spaceship00@naver.com>
2025-04-09 15:26:29 +0000

    [AutoMatch] Prefer playing with various people

--- wasm/match_generator.cpp
+++ wasm/match_generator.cpp
@@ -55,6 +55,8 @@ struct PlayerGameStats
     int with_weak_count;
     int with_similar_count;
     int with_strong_count;
+    std::unordered_map<std::string, int> partner_count;
+    std::unordered_map<std::string, int> players_in_same_game_count;
 };
 
 std::random_device rd;
@@ -195,14 +197,16 @@ int score_players(const std::vector<Row> &games)
         for (const auto &game : row)
         {
             float average_level = get_average_level(game);
-            for (const auto &player : game)
+            for (int i = 0; i < game.size(); i++)
             {
+                const auto &player = game[i];
                 if (stats.find(player.name) == stats.end())
                 {
-                    stats.insert({player.name, {0, 0, 0}});
+                    stats.insert({player.name, {0, 0, 0, {}}});
                 }
-
                 auto &player_stats = stats[player.name];
+
+                // Score game level difficulty.
                 if (std::abs(player.level - average_level) <= COMPETE_LEVEL_THRESHOLD)
                 {
                     player_stats.with_similar_count++;
@@ -215,6 +219,29 @@ int score_players(const std::vector<Row> &games)
                 {
                     player_stats.with_weak_count++;
                 }
+
+                // Score duplicate partners.
+                const auto &partner = game[(i / 2) * 2 + (1 - (i % 2))];
+                if (player_stats.partner_count.find(partner.name) == player_stats.partner_count.end())
+                {
+                    player_stats.partner_count.insert({});
+                }
+                player_stats.partner_count[partner.name]++;
+
+                // Score duplicate players in same game.
+                for (int j = 0; j < game.size(); ++j)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    const auto &other = game[j];
+                    if (player_stats.players_in_same_game_count.find(other.name) == player_stats.players_in_same_game_count.end())
+                    {
+                        player_stats.players_in_same_game_count.insert({});
+                    }
+                    player_stats.players_in_same_game_count[other.name]++;
+                }
             }
         }
     }
@@ -224,8 +251,17 @@ int score_players(const std::vector<Row> &games)
     for (const auto &it : stats)
     {
         const auto &stat = it.second;
-        score += pow(2, stat.with_strong_count);
-        score += pow(3, stat.with_weak_count);
+        score += pow(1.5, stat.with_strong_count);
+        score += pow(2, stat.with_weak_count);
+
+        for (const auto &partner_it : stat.partner_count)
+        {
+            score += pow(3, partner_it.second - 1);
+        }
+        for (const auto &other_it : stat.players_in_same_game_count)
+        {
+            score += pow(1.5, other_it.second - 1);
+        }
     }
 
     return score;
