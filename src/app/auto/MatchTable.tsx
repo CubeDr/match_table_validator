8fb988cb77f33b3d36363fd164228e0ed8ee8363
Hyuni Kim <spaceship00@naver.com>
2025-04-09 14:27:58 +0000

    [AutoMatch] Highlight hovered name in table

--- src/app/auto/MatchTable.tsx
+++ src/app/auto/MatchTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './MatchTable.module.css';
 import { Table } from './page';
 
@@ -6,6 +7,8 @@ interface Props {
 }
 
 export default function MatchTable({ table }: Props) {
+    const [hovered, setHovered] = useState<string | null>(null);
+
     return (
         <div className={styles.TableWrapper}>
             <table className={styles.Table}>
@@ -15,9 +18,12 @@ export default function MatchTable({ table }: Props) {
                             {rowOfGames.flat().map((player, playerIndex) => (
                                 <td
                                     key={`player-${rowIndex}-${playerIndex}`}
+                                    onPointerEnter={() => setHovered(player)}
+                                    onPointerLeave={() => setHovered(null)}
                                     className={styles.TableCell
                                         + (playerIndex % 4 === 3 ? ' ' + styles.StrongBorderRight : '')
-                                        + (playerIndex % 4 === 1 ? ' ' + styles.DashedBorderRight : '')}>
+                                        + (playerIndex % 4 === 1 ? ' ' + styles.DashedBorderRight : '')
+                                        + (hovered === player ? ' ' + styles.Hovered : '')}>
                                     {player}
                                 </td>
                             ))}
