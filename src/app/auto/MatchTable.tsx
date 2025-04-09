2ef3ba8d785f6660861dc59d84d514334c80bb77
Hyuni Kim <spaceship00@naver.com>
2025-04-09 16:00:00 +0000

    [AutoMatch] Add copy button to the generated table

--- src/app/auto/MatchTable.tsx
+++ src/app/auto/MatchTable.tsx
@@ -9,28 +9,37 @@ interface Props {
 export default function MatchTable({ table }: Props) {
     const [hovered, setHovered] = useState<string | null>(null);
 
+    function copy() {
+        navigator.clipboard.writeText(JSON.stringify(table));
+    }
+
     return (
-        <div className={styles.TableWrapper}>
-            <table className={styles.Table}>
-                <tbody>
-                    {table.map((rowOfGames, rowIndex) => (
-                        <tr key={`row-${rowIndex}`}>
-                            {rowOfGames.flat().map((player, playerIndex) => (
-                                <td
-                                    key={`player-${rowIndex}-${playerIndex}`}
-                                    onPointerEnter={() => setHovered(player)}
-                                    onPointerLeave={() => setHovered(null)}
-                                    className={styles.TableCell
-                                        + (playerIndex % 4 === 3 ? ' ' + styles.StrongBorderRight : '')
-                                        + (playerIndex % 4 === 1 ? ' ' + styles.DashedBorderRight : '')
-                                        + (hovered === player ? ' ' + styles.Hovered : '')}>
-                                    {player}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <>
+            <div>
+                <button onClick={copy}>Copy</button>
+            </div>
+            <div className={styles.TableWrapper}>
+                <table className={styles.Table}>
+                    <tbody>
+                        {table.map((rowOfGames, rowIndex) => (
+                            <tr key={`row-${rowIndex}`}>
+                                {rowOfGames.flat().map((player, playerIndex) => (
+                                    <td
+                                        key={`player-${rowIndex}-${playerIndex}`}
+                                        onPointerEnter={() => setHovered(player)}
+                                        onPointerLeave={() => setHovered(null)}
+                                        className={styles.TableCell
+                                            + (playerIndex % 4 === 3 ? ' ' + styles.StrongBorderRight : '')
+                                            + (playerIndex % 4 === 1 ? ' ' + styles.DashedBorderRight : '')
+                                            + (hovered === player ? ' ' + styles.Hovered : '')}>
+                                        {player}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
