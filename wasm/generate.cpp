b1fc5ce86ec5ab010f78ce4cbe2266d67be88ea9
Hyuni <spaceship00@naver.com>
2025-04-10 23:21:42 +0900

    [AutoMatch] Use random

--- wasm/generate.cpp
+++ wasm/generate.cpp
@@ -11,6 +11,9 @@
 #include "score.h"
 #include "types.h"
 
+std::random_device rd1;
+std::mt19937 gen1(rd1());
+
 struct ThreadResult
 {
     score_t best_score = std::numeric_limits<score_t>::max();
@@ -153,16 +156,46 @@ bool hill_climb_best_among_all(MatchTable &match_table, int total_length)
     return true;
 }
 
+bool hill_climb_best_among_random(MatchTable &match_table, int total_length, int iterations)
+{
+    score_t original_score = score_games(match_table);
+    score_t best_score = original_score;
+    int best_index1 = 0;
+    int best_index2 = 0;
+
+    std::uniform_int_distribution<> distr(0, total_length - 1);
+    for (int i = 0; i < iterations; ++i)
+    {
+        int index1 = distr(gen1);
+        int index2 = distr(gen1);
+
+        score_t swapped_score = score_games(match_table, index1, index2);
+        if (swapped_score < best_score)
+        {
+            best_score = swapped_score;
+            best_index1 = index1;
+            best_index2 = index2;
+        }
+    }
+
+    if (best_score == original_score)
+    {
+        return false;
+    }
+
+    std::cout << "Swapping " << best_index1 << " and " << best_index2 << " => score: " << best_score << std::endl;
+    match_table.swap(best_index1, best_index2);
+    return true;
+}
+
 void hill_climb(MatchTable &match_table, int total_length, int max_iterations)
 {
     size_t num_threads = get_number_of_threads();
     for (int iteration = 0; iteration < max_iterations; ++iteration)
     {
         // if (!parallel_hill_climb_best_among_all(match_table, num_threads, total_length))
-        // {
-        //     break;
-        // }
-        if (!hill_climb_best_among_all(match_table, total_length))
+        // if (!hill_climb_best_among_all(match_table, total_length))
+        if (!hill_climb_best_among_random(match_table, total_length, 10000))
         {
             break;
         }
